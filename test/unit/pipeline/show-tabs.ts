86381e4f0b2d6d7acdd888704ca8ea25cc874a15
Filipe <52853539+oliverjiu@users.noreply.github.com>
2020-02-06 20:04:02 +0900

    Add: Unit test file

--- test/unit/pipeline/show-tabs.ts
+++ test/unit/pipeline/show-tabs.ts
@@ -1,7 +1,11 @@
 /*
-	File: src/test/unit/pipeline/show-tabs.ts
+	File: test/unit/pipeline/show-tabs.ts
 	cpuabuse.com
 */
+
+/**
+ * Tests `showTabs`.
+ */
 
 import { strictEqual } from "assert";
 //@ts-ignore
@@ -10,7 +14,7 @@ import { fruitTable as input } from "../../input/pipeline.js";
 import { fruitTable as expected } from "../../expected/pipeline.js";
 import { processTabs } from "../../../src/pipeline/show-tabs";
 
-function testShowTabs() {
+export function testShowTabs():void {
 	describe("Fruit table TSV string", function() {
 		it("should have tabs replaced", function() {
 			strictEqual(processTabs(input), expected);
