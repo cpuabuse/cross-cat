17236cc3d401b4720bfb836227dc60db45de7867
Yano <51319482+yanocv@users.noreply.github.com>
2020-02-18 19:12:23 +0900

    Add: Test for number-nonblank #3

--- test/unit/pipeline/number-nonblank.ts
+++ test/unit/pipeline/number-nonblank.ts
@@ -9,13 +9,13 @@
 
 import { strictEqual } from "assert";
 import { processNumberNonBlank } from "../../../src/pipeline/number-nonblank";
-import { food as input } from "../../input/pipeline";
-import { food as expected } from "../../expected/pipeline";
+import { animals as input } from "../../input/pipeline";
+import { animals as expected } from "../../expected/pipeline";
 
 // Test for multiple lines
 export function testNumberNonBlank(): void {
 	describe("number-nonblank", function() {
-		it("should add numbers to the begining of the lines", function() {
+		it("Number nonempty output lines, overrides -n", function() {
 			strictEqual(processNumberNonBlank(input), expected);
 		});
 	});
