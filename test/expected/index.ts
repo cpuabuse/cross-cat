5c076e908b03b0918d8726a8784b505d3884dfab
Herman <35882542+winterhotlatte@users.noreply.github.com>
2020-03-14 16:51:57 +0900

    Add: System tests #5

---
+++ test/expected/index.ts
@@ -0,0 +1,46 @@
+/*
+	File: test/expected/index.ts
+	cpuabuse.com
+*/
+
+/**
+ * .Provide the expected data.
+ */
+
+// Fruits list for system test.
+export const fruits: string = `Apple
+Orange
+Banana
+Blueberry
+Grapes
+Grape fruit`;
+
+export const fruitsNumber: string = `1 Apple
+2 Orange
+3 Banana
+4 Blueberry
+5 Grapes
+6 Grape fruit`;
+
+export const fruitsShowEnds: string = `Apple$
+Orange$
+Banana$
+Blueberry$
+Grapes$
+Grape fruit$`;
+
+export const vegetablesNumberNonBlank: string = `1 Carrot
+
+2 Pumpkin
+
+3 Potato
+
+4 Spinach
+
+5 Sweet potato`;
+
+export const dinosaursShowTabs: string = `Tyrannosaurus Rex^Ilength 45ft
+Triceratops^Ilength 26ft
+Velociraptor^Ilength 6ft
+Stegosaurus^Ilength 16ft
+Spinosaurus^Ilength 60ft`;
