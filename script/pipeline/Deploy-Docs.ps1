ef671f036d93b5e46aaa287837bcc4609a9ed313
Herman <35882542+winterhotlatte@users.noreply.github.com>
2020-03-15 20:05:07 +0900

    Publish: Documentation

--- script/pipeline/Deploy-Docs.ps1
+++ script/pipeline/Deploy-Docs.ps1
@@ -1,17 +1,27 @@
 #!/usr/bin/env pwsh
 
+<#
+	File: script\pipeline\Deploy-Docs.ps1
+	Author: cpuabuse.com
+	Description: Deploys to GitHub pages
+#>
+
+<#######################################################################################################################
+	Initialize
+#######################################################################################################################>
+
 # Start-Pipeline
 . $(Join-Path -Path $PSScriptRoot -ChildPath "common" "Start-Pipeline.ps1")
 
 # Set vars
-[ValidateNotNullOrEmpty()][String]$BuildPath = Join-Path -Path "build" -ChildPath "docs"
+[ValidateNotNullOrEmpty()][String]$BuildPath = Join-Path -Path "build" -ChildPath "typedoc"
 [ValidateNotNullOrEmpty()][String]$CommitMessage = "Publish: Documentation"
-[ValidateNotNullOrEmpty()][String]$DocsPath = "docs"
 [ValidateNotNullOrEmpty()][String]$GitDirectory = ".git"
 [ValidateNotNullOrEmpty()][String]$Here = "."
-[ValidateNotNullOrEmpty()][String]$JekyllPath = Join-Path -Path "docs" -ChildPath ".nojekyll"
+[ValidateNotNullOrEmpty()][String]$JekyllPath = Join-Path -Path $BuildPath -ChildPath ".nojekyll"
 [ValidateNotNullOrEmpty()][String]$MasterBranch = "master"
 [ValidateNotNullOrEmpty()][String]$PagesBranch = "gh-pages"
+[ValidateNotNullOrEmpty()][String]$RepoPath = Join-Path -Path "build" -ChildPath "docs"
 [ValidateNotNullOrEmpty()][String]$RepoPullURL = "https://github.com/cpuabuse/cross-cat"
 [ValidateNotNullOrEmpty()][String]$RepoPushURL = "https://$($env:GITHUB_PAT)@github.com/cpuabuse/cross-cat"
 [ValidateNotNullOrEmpty()][String]$UserName = "El Gato Bot"
@@ -20,32 +30,62 @@
 # Install-Dependencies
 & $Paths.InstallDependencies
 
-# Create directory
-New-Item -ItemType "Directory" -Path $DocsPath
+<#######################################################################################################################
+	Prepare repository
+#######################################################################################################################>
 
-# Set up git and undo commit
-Push-Location -Path $DocsPath
+# Create directory
+if (Test-Path -Path $RepoPath) {
+	# Force for hidden files
+	Remove-Item $RepoPath -Recurse -Force
+}
+New-Item -ItemType "Directory" -Path $RepoPath
+
+
+# Go to repo
+Push-Location -Path $RepoPath
+
+# Init repo
 git init; if (-not $?) { throw }
+
+# Config git
 git config user.name $UserName; if (-not $?) { throw }
 git config user.email $UserEmail; if (-not $?) { throw }
+
+# Create branch from dev
 git remote add origin $RepoPullURL; if (-not $?) { throw }
 git fetch; if (-not $?) { throw }
 git checkout $MasterBranch; if (-not $?) { throw }
 git branch $PagesBranch; if (-not $?) { throw }
 git checkout $PagesBranch; if (-not $?) { throw }
+
+# Remove all the files in repository; Force to remove potential hidden files
+Get-ChildItem -Path $Here -Exclude $GitDirectory -Force | Remove-Item -Recurse -Force
+
+# Go back to root
 Pop-Location
 
-# Remove all the files in case there is inconsistency; Force to remove potential hidden files
-Get-ChildItem -Path $DocsPath -Exclude $GitDirectory -Force | Remove-Item -Recurse -Force
+<#######################################################################################################################
+	Build
+#######################################################################################################################>
+
+# Empty build directory; Force to remove potential hidden files
+if (!(Test-Path -Path $BuildPath)) {
+	Get-ChildItem -Path $BuildPath -Exclude $GitDirectory -Force | Remove-Item -Recurse -Force
+}
 
 # Create docs
 npm run build:docs; if (-not $?) { throw }
 
 # Copy docs
-Get-ChildItem -Path $BuildPath | Copy-Item -Destination $DocsPath -Recurse
+Get-ChildItem -Path $BuildPath | Copy-Item -Destination $RepoPath -Recurse
 
 # Fix jekyll; Value seems to be required
 New-Item $JekyllPath -type "File" -Value ""
+
+<#######################################################################################################################
+	Push
+#######################################################################################################################>
 
 # Git
 Push-Location -Path $DocsPath
