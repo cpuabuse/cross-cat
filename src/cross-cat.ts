628ce628391638ec13d386f3247128ab6cc49569
Herman <35882542+winterhotlatte@users.noreply.github.com>
2020-03-17 05:28:08 +0900

    Add: License doc

--- src/cross-cat.ts
+++ src/cross-cat.ts
@@ -9,6 +9,17 @@
  * Entry point for the project.
  */
 
+/**
+ * @license ISC
+ * ISC License (ISC)
+ *
+ * Copyright 2020 cpuabuse.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 import { Command } from "commander";
 import getStdin from "get-stdin";
 import { readFile } from "fs";
