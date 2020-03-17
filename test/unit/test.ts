1d0cd93adf25f72296ad3ba60c16e7c5ec4c99ae
Yano <51319482+yanocv@users.noreply.github.com>
2020-03-17 19:21:57 +0900

    Add: Unit test to test

--- test/unit/test.ts
+++ test/unit/test.ts
@@ -7,6 +7,8 @@
  * Performs a series of unit tests.
  */
 
+import { testNumber } from "./pipeline/number";
+import { testNumberNonBlank } from "./pipeline/number-nonblank";
 import { testShowEnds } from "./pipeline/show-ends";
 import { testShowNonPrinting } from "./pipeline/show-nonprinting";
 import { testShowTabs } from "./pipeline/show-tabs";
@@ -16,6 +18,12 @@ import { testsqueezeBlank } from "./pipeline/squeezeBlank";
  * All of the unit tests.
  */
 export function unitTest(): void {
+	// number
+	testNumber();
+
+	// numberNonBlank
+	testNumberNonBlank();
+
 	// showNonpriting
 	testShowNonPrinting();
 
