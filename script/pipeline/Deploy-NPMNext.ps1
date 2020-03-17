d588846c0ad65d583ce014f28dd03765aace8101
Herman <35882542+winterhotlatte@users.noreply.github.com>
2020-03-18 08:26:36 +0900

    Add: NPM publish next outline

--- script/pipeline/Deploy-NPMNext.ps1
+++ script/pipeline/Deploy-NPMNext.ps1
@@ -4,8 +4,21 @@
 # Start-Pipeline
 . $(Join-Path -Path $PSScriptRoot -ChildPath "common" "Start-Pipeline.ps1")
 
+[ValidateNotNullOrEmpty()][String]$NPMRCValue = "//registry.npmjs.org/:_authToken=$env:NPM_TOKEN"
+[ValidateNotNullOrEmpty()][String]$NPMRCPath = ".npmrc"
+
+# Patch and push
+npm version patch --message "Release: Next v%s [ci skip]"; if (-not $?) { throw }
+git push; if (-not $?) { throw }
+
+# Replace NPM config
+Add-Content -Path $NPMRCPath -Value $NPMRCValue
+
 # Publish to NPM
-npm publish --tag next
+npm publish --tag next; if (-not $?) { throw }
+
+# Remove config with token, just in case
+Remove-Item -Path $NPMRCPath
 
 # Stop-Pipeline
 & $Paths.StopPipeline
