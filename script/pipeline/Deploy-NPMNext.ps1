4391844fe0c54d1d1b25a422999916ca49d98257
Herman <35882542+winterhotlatte@users.noreply.github.com>
2020-03-18 18:02:05 +0900

    Change: Git push from detached

--- script/pipeline/Deploy-NPMNext.ps1
+++ script/pipeline/Deploy-NPMNext.ps1
@@ -4,12 +4,13 @@
 # Start-Pipeline
 . $(Join-Path -Path $PSScriptRoot -ChildPath "common" "Start-Pipeline.ps1")
 
-[ValidateNotNullOrEmpty()][String]$NPMRCValue = "//registry.npmjs.org/:_authToken=$env:NPM_TOKEN"
+[ValidateNotNullOrEmpty()][String]$Branch = "dev"
+[ValidateNotNullOrEmpty()][String]$NPMRCValue = "//registry.npmjs.org/:_authToken=$($env:NPM_TOKEN)"
 [ValidateNotNullOrEmpty()][String]$NPMRCPath = ".npmrc"
 
 # Patch and push
 npm version patch --no-git-tag-version --message "Release: Next v%s [ci skip]"; if (-not $?) { throw }
-git push; if (-not $?) { throw }
+git push origin "HEAD:$($Branch)"; if (-not $?) { throw }
 
 # Replace NPM config
 Add-Content -Path $NPMRCPath -Value $NPMRCValue
