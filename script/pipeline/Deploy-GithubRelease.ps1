12e61ed12d3a1dc7e6ca196a28bc037c7ec49630
Herman <35882542+winterhotlatte@users.noreply.github.com>
2020-03-19 22:30:39 +0900

    Add: Release history

--- script/pipeline/Deploy-GithubRelease.ps1
+++ script/pipeline/Deploy-GithubRelease.ps1
@@ -10,12 +10,26 @@
 # Variables
 [ValidateNotNullOrEmpty()][String]$GithubOrganization = "cpuabuse"
 [ValidateNotNullOrEmpty()][String]$GithubRepository = "cross-cat"
+[ValidateNotNullOrEmpty()][String]$RepoPushURL = "https://$($env:GITHUB_PAT)@github.com/cpuabuse/cross-cat"
+[ValidateNotNullOrEmpty()][String]$UserName = "El Gato Bot"
+[ValidateNotNullOrEmpty()][String]$UserEmail = "60073838+elgatobot@users.noreply.github.com"
 [ValidateNotNullOrEmpty()][String]$Version = (Get-Content -Path $PackageFilePath | ConvertFrom-Json).version; `
 	[ValidateNotNullOrEmpty()][String]$Tag = "v$Version"; `
-	[ValidateNotNullOrEmpty()][String]$Message = "Release: Next $Tag"
+	[ValidateNotNullOrEmpty()][String]$Message = "Release: $Tag
+	
+	$(git log --oneline --no-decorate release..HEAD | Out-String; if (-not $?) { throw })
+	"
+
+# Config git
+git config user.name $UserName; if (-not $?) { throw }
+git config user.email $UserEmail; if (-not $?) { throw }
 
 # Publish release
-npx github-release upload --token $env:GITHUB_PAT --owner $GithubOrganization --repo $GithubRepository --tag $Tag --name $Tag --body $Message
+npx github-release upload --token $env:GITHUB_PAT --owner $GithubOrganization --repo $GithubRepository --tag $Tag --name $Tag --body $Message; if (-not $?) { throw }
+
+# Retag
+git tag release --force; if (-not $?) { throw }
+git push $RepoPushURL release --force; if (-not $?) { throw }
 
 # Stop-Pipeline
 & $Paths.StopPipeline
