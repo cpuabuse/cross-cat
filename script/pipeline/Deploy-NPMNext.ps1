01d3078adf71ede1d5f8551877f6ac8c292c906c
Herman <35882542+winterhotlatte@users.noreply.github.com>
2020-03-19 10:21:02 +0900

    Add: Appropriate tags

--- script/pipeline/Deploy-NPMNext.ps1
+++ script/pipeline/Deploy-NPMNext.ps1
@@ -5,13 +5,15 @@
 . $(Join-Path -Path $PSScriptRoot -ChildPath "common" "Start-Pipeline.ps1")
 
 [ValidateNotNullOrEmpty()][String]$Branch = "dev"
+[ValidateNotNullOrEmpty()][String]$Message = "Release"
 [ValidateNotNullOrEmpty()][String]$NPMRCValue = "//registry.npmjs.org/:_authToken=$($env:NPM_TOKEN)"
 [ValidateNotNullOrEmpty()][String]$NPMRCPath = ".npmrc"
 [ValidateNotNullOrEmpty()][String]$PackageFilePath = "package.json"
 [ValidateNotNullOrEmpty()][String]$RepoPushURL = "https://$($env:GITHUB_PAT)@github.com/cpuabuse/cross-cat"
 [ValidateNotNullOrEmpty()][String]$UserName = "El Gato Bot"
 [ValidateNotNullOrEmpty()][String]$UserEmail = "60073838+elgatobot@users.noreply.github.com"
-[ValidateNotNullOrEmpty()][String]$Tag = "v0.0.1"
+[ValidateNotNullOrEmpty()][String]$Version = "v0.0.1"; `
+	[ValidateNotNullOrEmpty()][String]$Tag = "v$Version"
 
 # Config git
 git config user.name $UserName; if (-not $?) { throw }
@@ -20,14 +22,17 @@ git config user.email $UserEmail; if (-not $?) { throw }
 # Git checkout
 git checkout $Branch
 
-# Patch
+# Patch, commit and tag
 npm version patch --no-git-tag-version; if (-not $?) { throw }
-$Tag = "v$((Get-Content -Path $PackageFilePath | ConvertFrom-Json).version)"
-git commit --all --message "Release: Next $Tag" --message "[ci skip]"
-git tag $Tag
+$Version = (Get-Content -Path $PackageFilePath | ConvertFrom-Json).version
+$Tag = "v$Version"
+$Message = "Release: Next $Tag"
+git commit --all --message $Message --message "[ci skip]"; if (-not $?) { throw }
+git tag $Tag --message $Message; if (-not $?) { throw }
 
 # Push
 git push $RepoPushURL; if (-not $?) { throw }
+git push $RepoPushURL $Tag; if (-not $?) { throw }
 
 # Replace NPM config
 Add-Content -Path $NPMRCPath -Value $NPMRCValue
